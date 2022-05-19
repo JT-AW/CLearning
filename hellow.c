6c4706aa7dc385d84f62482f17e962b54bd8b887
John Wang <john-w@berkeley.edu>
2022-05-18 18:47:31 -0700

    hellow 2.0

--- hellow.c
+++ hellow.c
@@ -4,6 +4,26 @@
 */
 #include <stdio.h>
 
+int double_it(int x);
+
+struct point {
+    int x;
+    int y;
+};
+
+struct tip {
+    int x;
+};
+
 int main(int argc, char *argv[]) {
-    printf("Hello world!\n");
+    printf("Hello world!\n%u", 56 || 12);
+    int numbers[3];
+    numbers[0] = 1;
+    printf("%d %d", double_it(3), numbers[0]);
+    struct point x;
+    
+}
+
+int double_it(int x) {
+    return x * 2;
 }
